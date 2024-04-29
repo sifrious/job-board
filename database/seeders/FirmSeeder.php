66c4eae2375f8e4f97353c4654a0c34a8c23ab61
mray <sifrious@mrays-MacBook-Pro.local>
2024-04-29 14:06:10 -0400

    added firms model

---
+++ database/seeders/FirmSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class FirmSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
